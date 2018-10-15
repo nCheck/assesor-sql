134722c268c2b018b24d8ce315fbd115aa95e3c9
Nehal Kalnad <yenwiikae@gmail.com>
2018-10-15 09:25:22 +0530

    nothin

---
+++ routes/auth.js
@@ -0,0 +1,21 @@
+var authController = require('../controllers/auth.ctrlr')
+
+module.exports = function(app, passport) {
+ 
+    app.get('/signup', authController.signup);
+ 
+ 
+    app.get('/signin', authController.signin);
+ 
+ 
+    app.post('/signup', passport.authenticate('local-signup', {
+            successRedirect: '/dashboard',
+ 
+            failureRedirect: '/signup'
+        }
+ 
+    ));
+ 
+ 
+ 
+}
